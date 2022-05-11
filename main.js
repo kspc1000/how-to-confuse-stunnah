9391dd164ed18cc535608f5d1575364dd0b38e5c
kspc1000 <kspc1000@gmail.com>
2022-05-11 23:54:04 +0800

    Edited main.js

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-console.log(document.getElementById("confuse_stunnah_table"));
 const words = [
-    "can't", "ain't", "couldn't",
-    "musn't", "meant", "don't",
-    "isn't", "wasn't", "needn't",
-    "aren't", "doesn't", "hasn't",
-    "haven't", "hadn't", "shan't",
-    "didn't", //"mightn't",
-].sort();
-const length = words.length;
-const numberOfColumns = Math.sqrt(words.length);
+    "ain't",  "aren't", "can't",  "couldn't",
+    "didn't", "doesn't", "don't",  "hadn't",
+    "hasn't", "haven't", "isn't",  'meant',
+    "musn't", "needn't", "shan't", "wasn't"
+]
+const { length: numberOfWords } = words;
+const numberOfColumns = Math.sqrt(numberOfWords);
+
 const table = document.getElementById("confuse_stunnah_table");
 let htmlContent = "";
-for (let i = 0; i < length; i++) {
+for (let i = 0; i < numberOfWords; i++) {
     if (i % numberOfColumns === 0) {
         htmlContent += "<tr></tr>"
     }
     htmlContent += `<td>${words[i] === "meant" ? `<em>meant</em>` : words[i]}</td>`;
 }
 table.innerHTML += htmlContent;
-console.log(table.innerHTML)
